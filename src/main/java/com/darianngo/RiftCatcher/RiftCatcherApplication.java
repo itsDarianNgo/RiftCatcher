41488c5f694b614e01283834ebbc30cf655e97bd
itsdarianngo <darianngo@gmail.com>
2023-09-05 04:10:42 -0500

    first commit

---
+++ src/main/java/com/darianngo/RiftCatcher/RiftCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.darianngo.RiftCatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiftCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiftCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darianngo/RiftCatcher/RiftCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darianngo.RiftCatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiftCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
